689c517cacb86b85855929ba4d2d3d98ed845f9b
cjunior-dino <cmbj32@gmail.com>
2024-05-03 18:43:14 -0400

    refactor (importa_arquivo): foi alterado o local de busca do arquivo csv

--- funcoes/importa_arquivo.py
+++ funcoes/importa_arquivo.py
@@ -1,7 +1,9 @@
 produto = []
-with open('dados.csv', 'r',encoding='utf-8') as arquivo:
-        next(arquivo)
-        lista_produtos = arquivo.readlines()
-        #criado a lista de produto
-        for i in lista_produtos:
-            produto.append(i.strip('\n').split(';'))
+
+with open('banco_de_dados\produtos\dados.csv', 'r',encoding='utf-8') as arquivo:
+    next(arquivo)
+    lista_produtos = arquivo.readlines()
+    #criado a lista de produto
+    for i in lista_produtos:
+        produto.append(i.strip('\n').split(';'))
+
