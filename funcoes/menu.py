d0130581340e0d11753e568ff770e83109af7d1d
cjunior-dino <cmbj32@gmail.com>
2024-05-03 19:07:43 -0400

    refactor (menu.py): foi alterado a mensagem da opcao 4

--- funcoes/menu.py
+++ funcoes/menu.py
@@ -4,5 +4,5 @@ def menu():
     print('1- FAZER NOVA BUSCA')
     print('2- ADICIONAR ITEM AO CARRINHO')
     print('3- VISUALIZAR OS ITENS DO CARRINHO')
-    print('4- FINALIZAR COMPRA')
+    print('4- FINALIZAR VENDA OU SAIR DO SISTEMA')
     print(' ')
