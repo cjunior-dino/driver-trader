232dd40486b6017c785b4c723f1be99f7f826cc4
unknown <cmbj32@gmail.com>
2024-06-24 16:14:16 -0400

    alterado o importa arquivos

--- funcoes/atualiza_dados.py
+++ funcoes/atualiza_dados.py
@@ -19,11 +19,13 @@ def atualiza_relatorio():
     with open(r'banco_de_dados\relatorio\relatorio.csv', 'w',encoding='utf-8') as arquivo:
         arquivo.write('codigo' + ';' + 'nome' + ';' + 'marca' + ';' + 'categoria' + ';' + 'quantidade' + ';' + 'preco' + ';'+ 'total' +'\n')
         for i in relatorio:
+            print("entrou i")
             for j in carrinho:
+                print("entrou j")
                 codigo, nome, marca, categoria, quantidade, preco = i
                 codigo2, nome2, marca2, categoria2, quantidade2, preco2 = j
                 if codigo2 == codigo and quantidade != quantidade2:
-                    total = int(quantidade) - int(quantidade2)
+                    total = int(quantidade) + int(quantidade2)
                     arquivo.write(codigo + ';' + nome + ';' + marca + ';' + categoria + ';' + str(total) + ';' + preco + '\n')
                     break
             else:
--- funcoes/importa_arquivo.py
+++ funcoes/importa_arquivo.py
@@ -10,12 +10,15 @@ def adicina():
             produto.append(i.strip('\n').split(';'))
     return produto
 
+
+"""Greração do relatorio de vendas"""
 def relatori_prod():
     relatorio.clear()
-    with open(r'banco_de_dados\produtos\dados.csv', 'r',encoding='utf-8') as arquivo:
+    with open(r'banco_de_dados\relatorio\relatorio.csv', 'r',encoding='utf-8') as arquivo:
         next(arquivo)
         lista_produtos = arquivo.readlines()
         #criado a lista de produto
         for i in lista_produtos:
             produto.append(i.strip('\n').split(';'))
+        print(relatorio)
     return relatorio
