ec49ecfd0105526f304827b1dbb0bf8f9f40ce93
unknown <cmbj32@gmail.com>
2024-06-28 16:16:13 -0400

    alterado

--- funcoes/atualiza_relatorio.py
+++ funcoes/atualiza_relatorio.py
@@ -3,14 +3,13 @@ from funcoes.importa_relatorio import *
 
 def atualiza_relatorio(): 
     print("atualiza")
+    relatorio_prod()
     with open(r'banco_de_dados\relatorio\relatorio.csv', 'w',encoding='utf-8') as arquivo:
         arquivo.write('codigo' + ';' + 'nome' + ';' + 'marca' + ';' + 'categoria' + ';' + 'total_venda' + ';' + 'valor_total' + '\n')
         for dados in relatorio2:
             print("relatorio")
-
-            print(relatorio2)
             for dado in carrinho:
-                print("relatorio")
+                print("carrinho")
                 codigo, nome, marca, categoria, quantidade, total = dados
                 codigo2, nome2, marca2, categoria2, quantidade2, total2 = dado
                 total_quant = 0
@@ -18,8 +17,8 @@ def atualiza_relatorio():
                 if codigo == codigo2:
                         total_quant = int(quantidade) + int(quantidade2)
                         total_venda = float(total) + (float(quantidade2)*float(total2))
-                        arquivo.write(str(codigo2) + ';'+ nome2 + ';' + marca2 + ';' + categoria2 + ';' + str(total_quant) + ';' + str(total_venda) + '\n')
+                        arquivo.write(str(codigo) + ';'+ nome + ';' + marca + ';' + categoria + ';' + str(total_quant) + ';' + str(total_venda) + '\n')
                         break
                 else:
-                    total_venda = float(quantidade)*float(total)
-                    arquivo.write(codigo + ';' + nome + ';' + marca + ';' + categoria + ';' + str(quantidade) + ';' + str(total_venda) + '\n')
+                    total_venda = float(quantidade2)*float(total2)
+                    arquivo.write(codigo2 + ';' + nome2 + ';' + marca2 + ';' + categoria2 + ';' + str(quantidade2) + ';' + str(total_venda) + '\n')
