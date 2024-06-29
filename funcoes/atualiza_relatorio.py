40cf399afdf17d0b6fbb6bbb7514ead169e4147c
unknown <cmbj32@gmail.com>
2024-06-29 12:01:08 -0400

    alterado novamente

--- funcoes/atualiza_relatorio.py
+++ funcoes/atualiza_relatorio.py
@@ -6,13 +6,12 @@ def atualiza_relatorio():
     relatorio_prod()
     with open(r'banco_de_dados\relatorio\relatorio.csv', 'w',encoding='utf-8') as arquivo:
         arquivo.write('codigo' + ';' + 'nome' + ';' + 'marca' + ';' + 'categoria' + ';' + 'total_venda' + ';' + 'valor_total' + '\n')
-        for dado in carrinho:
-            codigo2, nome2, marca2, categoria2, quantidade2, total2 = dado
+        for dados in relatorio2:
             print("relatorio")
-            for dados in relatorio2:
+            for dado in carrinho:
                 print("carrinho")
                 codigo, nome, marca, categoria, quantidade, total = dados
-                
+                codigo2, nome2, marca2, categoria2, quantidade2, total2 = dado
                 total_quant = 0
                 total_venda = 0       
                 if codigo == codigo2:
@@ -20,6 +19,3 @@ def atualiza_relatorio():
                         total_venda = float(total) + (float(quantidade2)*float(total2))
                         arquivo.write(str(codigo) + ';'+ nome + ';' + marca + ';' + categoria + ';' + str(total_quant) + ';' + str(total_venda) + '\n')
                         break
-            else:
-                total_venda = float(quantidade2)*float(total2)
-                arquivo.write(codigo2 + ';' + nome2 + ';' + marca2 + ';' + categoria2 + ';' + str(quantidade2) + ';' + str(total_venda) + '\n')
